6ac6dd882dfbc642d226e47e95521c98aa43d0f3
20212PF.CC0020 <20212PF.CC0020@pc-253371.passofundo.ifsul.local>
2024-03-20 11:08:22 -0300

    Criação de Projeto

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/compras/lpoo_sistemacompras/TestPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.compras.lpoo_sistemacompras;
+
+/**
+ *
+ * @author 20212PF.CC0020
+ */
+public class TestPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
